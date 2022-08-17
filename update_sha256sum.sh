7c106d057428a0f4ad49761090807a014e9fc5a6
Nicolas Iooss <nicolas.iooss@ledger.fr>
2022-08-17 13:16:29 -0700

    Add SHA256SUM.txt
    This makes it easier to gather new firmware

---
+++ update_sha256sum.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Update SHA256SUM.txt
+set -eu
+
+cd "$(dirname -- "$0")"
+
+find intel_bluetooth intel_wifi -type f | sort | xargs sha256sum > SHA256SUM.txt
+
+# Find duplicate entries in wifi
+grep -v '  intel_bluetooth/' < SHA256SUM.txt | cut -d ' ' -f 1 | LANG=C sort | uniq -c | grep -v '^ *1 ' |sort -n | \
+while IFS= read -r LINE ; do
+    printf "%s\n" "$LINE"
+    SHA="$(printf %s "$LINE" | awk '{print $2;}')"
+    grep --color "^$SHA " < SHA256SUM.txt
+done
